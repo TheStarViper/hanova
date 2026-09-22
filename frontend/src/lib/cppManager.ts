6968df1dc9456926eaefe5f36561b04e20fec8df
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-09-21 22:20:08 -0400

    temporarily comment out lines, to prevent build error

--- frontend/src/lib/cppManager.ts
+++ frontend/src/lib/cppManager.ts
@@ -1,9 +1,9 @@
-import MainModuleFactory from "./cpp/cpp_module";
+// import MainModuleFactory from "./cpp/cpp_module";
 
 export class CppManager {
 	constructor() {}
 
 	public async init() {
-		const Module = await MainModuleFactory();
+		// const Module = await MainModuleFactory();
 	}
 }
