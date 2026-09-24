fc67f4ffdcc29047d6bcb98203f7ac8379bcb2e3
TheStarViper <andrewwangnew@gmail.com>
2026-09-24 09:54:35 -0700

    wait can i commit offline test

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -12,4 +12,19 @@
 
 extern "C"{
 
+    int allocate_buffer(int size){
+
+    }
+
+    void free_buffer(int* pointer){
+        free(pointer);
+    }
+
+    int convert(int input, int input_length){
+        int output = (int*)malloc(input_length);
+        for (int i = 0; i<input_length; i++;){
+            output[i] = ~input[i]
+        }
+        return output;
+    }
 }
